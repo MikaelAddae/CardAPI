20107a4503a6f74a36e52c7a5fac22d256af5f98
Mikael Addae <mikael.addae@gmail.com>
2023-05-16 08:45:19 -0400

    CardsAPI

---
+++ DeckOfCards/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using DeckOfCards.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeckOfCards.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        CardsDAL cardAPI = new CardsDAL();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult DrawCards()
+        {
+            Deck newDeck = cardAPI.GetDeck();
+            return RedirectToAction("DealtHand", new {id = newDeck.deck_id });
+        }
+
+
+        public IActionResult DealtHand(string id)
+        {
+            Hand newHand = cardAPI.GetHand(id);
+            return View(newHand);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DeckOfCards/Models/CardsDAL.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp;
+
+namespace DeckOfCards.Models
+{
+    public class CardsDAL
+    {
+        public Deck GetDeck()
+        {
+            string URL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+            RestClient client = new RestClient(URL);
+            RestRequest request = new RestRequest();
+
+            Deck response = client.Get<Deck>(request);
+
+            return response;
+        }
+
+        public Hand GetHand(string id)
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/{id}/draw/?count=5";
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest();
+
+            Hand response = client.Get<Hand>(request);
+
+            return response;
+        }
+
+
+
+    }
+}
